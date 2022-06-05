084d5b9472279278cad44fb9632a64f57144a0a9
jhk48 <imjaehyeon48@gmail.com>
2022-06-05 13:04:40 +0900

    refactor: 네이버 API를 사용하여 번역하는 함수 이름을 naverTranslate로 수정

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,5 @@
 import { commands, ExtensionContext, Range, window } from 'vscode';
-import translate from './lib/translate';
+import naverTranslate from './lib/naverTranslate';
 
 export function activate(context: ExtensionContext) {
 	const disposable = commands.registerCommand('kortoeng.kortoeng', async () => {
@@ -10,23 +10,20 @@ export function activate(context: ExtensionContext) {
 		const selections = editor.selections;
 		const range = new Range(selections[0].start, selections[selections.length - 1].end);
 		const highlightedText = editor.document.getText(range);
-		try {
-			if (highlightedText.trim() === '') {
-				return;
-			}
-			if (highlightedText.length > 5000) {
-				window.showErrorMessage('최대 5,000자 까지 번역할 수 있습니다.');
-				return;
-			}
-			const translatedText = await translate(highlightedText);
-			const pickedItem = await window.showQuickPick([translatedText]);
-			if (!pickedItem) {
-				return;
-			}
-			editor.edit(edit => edit.replace(range, pickedItem));
-		} catch (error) {
-			console.log(error);
+		if (highlightedText.trim() === '') {
+			return;
 		}
+		if (highlightedText.length > 5000) {
+			window.showErrorMessage('최대 5,000자 까지 번역할 수 있습니다.');
+			return;
+		}
+
+		const translatedText = await naverTranslate(highlightedText);
+		const pickedItem = await window.showQuickPick([translatedText]);
+		if (!pickedItem) {
+			return;
+		}
+		editor.edit(edit => edit.replace(range, pickedItem));
 	});
 
 	context.subscriptions.push(disposable);
